0998b027a89b716029d925b783ead982a2569faa
Juan Gonzalez <jrg2156@gmail.com>
2018-07-18 01:39:49 -0400

    Refactor; add yadm status segment

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,15 +1,7 @@
 function fish_prompt
-    set -l last_command_status $status
-
     set -l arrow_top_left '╭─'
     set -l arrow_bottom_left '╰ '
     set -l divider_icon ''
-
-    set -l normal_color (set_color normal)
-    set -l black_text (set_color black)
-    set -l blue_text (set_color blue)
-
-    set cwd (prompt_pwd)
 
     set -l os_icon
     switch (uname)
@@ -20,36 +12,47 @@ function fish_prompt
       case Windows_NT
         set os_icon ''
     end
+    set cwd (prompt_pwd)
 
-    set -l texts       $os_icon  $cwd
-    set -l _bg_colors  black     blue
-    set -l _fg_colors  white     black
+    set -l normal_color (set_color normal)
+    set -l blue_text (set_color blue)
+
+    set -l segments    $os_icon  $cwd
+    set -l bg_colors  black     blue
+    set -l fg_colors  white     black
 
     if git_is_repo
-        set texts $texts (git_branch_name)
-        set _fg_colors $_fg_colors black
+        set segments $segments (git_branch_name)
+        set fg_colors $fg_colors black
         if git_is_touched
-          set _bg_colors $_bg_colors yellow
+          set bg_colors $bg_colors yellow
         else
-          set _bg_colors $_bg_colors green
+          set bg_colors $bg_colors green
         end
     end
 
-    for i in (seq (count $texts))
-      set bg_colors $bg_colors (set_color -b $_bg_colors[$i])
-      set fg_colors $fg_colors (set_color $_fg_colors[$i])
+    set -l yadm_status
+    switch $YADM_STATE
+      case 1
+        set yadm_status 'Yadm '
+      case 2
+        set yadm_status 'Yadm '
     end
 
-    echo -n -s $normal_color
-    echo -n -s $blue_text $arrow_top_left
-    echo -n -s $fg_colors[1] $bg_colors[1] " $texts[1] "
-    for i in (seq (count $texts))[2..-2]
-        echo -n -s (set_color $_bg_colors[(math "$i - 1")]) $bg_colors[$i] $divider_icon
-        echo -n -s $fg_colors[$i] $bg_colors[$i] " $texts[$i] "
+    if [ $yadm_status != 0 ]
+        set  segments   $segments   $yadm_status
+        set  bg_colors  $bg_colors  black
+        set  fg_colors  $fg_colors  magenta
     end
-    echo -n -s (set_color $_bg_colors[-2]) $bg_colors[-1] $divider_icon
-    echo -n -s $fg_colors[-1] $bg_colors[-1] " $texts[-1] "
-    echo -n -s $normal_color (set_color $_bg_colors[-1]) $divider_icon
-    echo -n -s -e $normal_color "\n" $blue_text $arrow_bottom_left
-    echo -n -s $normal_color
+
+    set -l divider_bgs $bg_colors[2..(count $bg_colors)] normal
+    set -l divider_fgs $bg_colors
+
+    # Prompt elements
+    echo -n -s $normal_color $blue_text $arrow_top_left
+    for i in (seq (count $segments))
+      echo -n -s (set_color -b $bg_colors[$i]) (set_color $fg_colors[$i]) " $segments[$i] "
+      echo -n -s $normal_color (set_color -b $divider_bgs[$i]) (set_color $divider_fgs[$i]) $divider_icon
+    end
+    echo -n -s -e $normal_color "\n" $blue_text $arrow_bottom_left $normal_color
 end
